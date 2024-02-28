308ddaebf3fc363e76fc2d02bcc671e70a0fcb99
Ninjazet <castellanostejada2001@gmail.com>
2024-02-27 20:01:58 -0600

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+
+            
+
+           
+        }
+    }
+
+}
